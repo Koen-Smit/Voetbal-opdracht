e5f212e1a2bd5c9e1b416fd10fe43451bab71dac
calthan leander <calsonleander99@gmail.com>
2022-12-14 14:04:32 +0100

    added routes for admin

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\teamController;
+use App\Http\Controllers\userController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +26,9 @@ Route::post('store', [teamController::class, 'store'])->name('store');
 Route::get('edit/{id}', [teamController::class, 'edit'])->middleware(['auth', 'verified'])->name('edit');
 
 Route::post('update', [teamController::class, 'update'])->middleware(['auth', 'verified'])->name('update');
+
+Route::get('admin', [userController::class, 'admin'])->middleware(['auth', 'verified'])->name('admin');
+Route::get('adminUpdate/{id}', [userController::class, 'adminUpdate'])->middleware(['auth', 'verified'])->name('adminUpdate');
 
 Route::delete('delete/{id}', [teamController::class, 'delete'])->name('delete');
 
