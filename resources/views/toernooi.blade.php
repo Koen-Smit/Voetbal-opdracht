973ae83a70caadd17a4c8eb405b66f9fd5234bc6
calthan leander <calsonleander99@gmail.com>
2022-12-20 10:40:23 +0100

    added toernooi page

--- resources/views/toernooi.blade.php
+++ resources/views/toernooi.blade.php
@@ -6,6 +6,38 @@
     </x-slot>
 
     <div class="py-12">
-
+        {{-- link to generate --}}
+        <a href="{{route('generate')}}" class="btn btn-primary">Generate</a>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>team 1</th>
+                    <th>team 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($matches as $match)
+                    <tr style="vertical-align: middle;">
+                        @foreach ($teams as $team)
+                            
+                                @if ($match->team1_id == $team->id)
+                                <td>
+                                {{$team->name}}
+                            </td>
+                                @endif
+                            
+                            
+                                @if ($match->team2_id == $team->id)
+                                <td>
+                                {{$team->name}}
+                            </td>
+                                @endif
+                                
+                        @endforeach
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        
     </div>
 </x-app-layout>
